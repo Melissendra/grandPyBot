290175cdf63121aebeed1da7af9aaf62f53ebf39
Melissendra <melissendra@gmail.com>
2020-05-28 16:14:42 +0200

    test for google_api begun

--- app/google_api.py
+++ app/google_api.py
@@ -1,4 +1,5 @@
 import googlemaps
+import re
 import app.constants as c
 
 """Class to get information from googlemaps Api"""
@@ -13,10 +14,21 @@ class GoogleMaps:
         gmaps = googlemaps.Client(key=self.api_key)
         gmap_result = gmaps.geocode(search, region='fr')
 
-        print(gmap_result)
+        try:
+            address = gmap_result[0]["formatted_address"]
+            address = re.sub(r"[,]", "", address)
+            latitude = gmap_result[0]["geometry"]["location"]["lat"]
+            longitude = gmap_result[0]["geometry"]["location"]["lng"]
+            return {
+                address,
+                latitude,
+                longitude
+            }
+        except IndexError:
+            return "No result"
+
 
 if __name__ == "__main__":
     google = GoogleMaps(c.API_KEY)
     gmaps = google.get_address("35b rue du petit pont, Meursac")
-        
-    
+    print(gmaps)
--- tests/test_google_api.py
+++ tests/test_google_api.py
@@ -1,1 +1,17 @@
-import envkey
+from app.google_api import GoogleMaps
+
+
+class GoogleMapsTest:
+
+    def test_get_address(self, monckeypatch):
+        return {
+            'address': "Meursac",
+            'latitude': 45.6460494,
+            'longitude': -0.7911047999999999
+        }
+
+        def monckeyReturn(self):
+            return result
+
+
+
