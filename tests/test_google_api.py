3a3d6e46fec3cfd4a53497790a11794a62a6e95c
Melissendra <melissendra77@gmail.com>
2020-05-29 09:52:53 +0200

    some little modifications

--- tests/test_google_api.py
+++ tests/test_google_api.py
@@ -3,15 +3,13 @@ from app.google_api import GoogleMaps
 
 class GoogleMapsTest:
 
-    def test_get_address(self, monckeypatch):
+    def test_get_address(self):
         return {
             'address': "Meursac",
             'latitude': 45.6460494,
             'longitude': -0.7911047999999999
         }
 
-        def monckeyReturn(self):
-            return result
 
 
 
