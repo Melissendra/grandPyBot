da79da1d2c89f518bdc5c2b6774b1f99ce4ac4f1
Melissendra <melissendra77@gmail.com>
2020-06-22 12:14:19 +0200

    bug js resolved

--- papybot/static/js/script.js
+++ papybot/static/js/script.js
@@ -2,8 +2,10 @@ let form = document.querySelector(".search");
 
 form.addEventListener("submit", function(event){
     event.preventDefault();
-    console.log("je marche");
-    const input = document.getElementById("grandPyBot");
+    const input = document.querySelector("#grandPybot");
     let inputVal = input.value;
-    console.log(inputVal);
+    const firstSentence = document.querySelector(".sentence");
+    const newDiv = new CreateElement("div", {"class": "userSentence"}, firstSentence).newElement();
+    const newUser = new CreateElement("p", {"class": "sentence"}, newDiv, "User").newElement();
+    const newQuestion = new CreateElement("p", {"class": "sentence"}, newUser, inputVal).newElement();
 });
